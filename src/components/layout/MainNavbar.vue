286affdab653ae92ab5aa0d0b85fc7b38ff9facb
Adham-Niazy <adhamniazy@gmail.com>
2024-11-20 09:51:50 +0200

    Basic Navbar Layout

---
+++ src/components/layout/MainNavbar.vue
@@ -0,0 +1,28 @@
+<script>
+import DynamicButton from "../shared/DynamicButton.vue";
+export default {
+  components: {
+    DynamicButton,
+  },
+  methods: {
+    handleClicked() {
+      console.log("CLICKED");
+    },
+  },
+};
+</script>
+
+<template>
+  <nav
+    class="flex justify-between items-center px-12 py-5 border-b border-[#EEE]"
+  >
+    <h2 class="font-light">Reservations</h2>
+    <DynamicButton
+      @buttonClicked="handleClicked"
+      btnStyle="main"
+      @click="handleClicked"
+    >
+      Disable Reservation
+    </DynamicButton>
+  </nav>
+</template>
