f6854c789ba3a3874b59c89c3df7eff89eb580d2
Adham-Niazy <adhamniazy@gmail.com>
2024-11-20 09:51:23 +0200

    basic implementation to (Reusable Dynamic Button)

---
+++ src/components/shared/DynamicButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ["btnStyle"],
+  computed: {
+    appliedBtnStyles() {
+      switch (this.btnStyle) {
+        case "main":
+          return "bg-violet-700 rounded-md text-white";
+        default:
+          return "";
+      }
+    },
+  },
+  methods: {
+    handleClickEmitter() {
+      this.$emit("buttonClicked");
+    },
+  },
+};
+</script>
+
+<template>
+  <button
+    @click="handleClickEmitter"
+    :class="`${appliedBtnStyles} px-4 py-2 cursor-pointer`"
+  >
+    <slot></slot>
+  </button>
+</template>
