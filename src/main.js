887bbab8b32be269d8204d1fd27ecc1e2a25a12c
Adham-Niazy <adhamniazy@gmail.com>
2024-11-21 02:03:38 +0200

    Prepare global variables and interceptors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
 import './index.css'
+
+// Setting Global Axios
+axios.defaults.baseURL = 'https://api.foodics.dev/v5'
+axios.interceptors.request.use((config) => {
+  const token = process.env.VUE_APP_AUTH_TOKEN;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+});
+axios.interceptors.response.use((response) => {
+  return response.data;
+}, (error) => {
+  console.error(error)
+  return Promise.reject(error)
+})
 
 Vue.config.productionTip = false
 
