8c2a42e90cb063df64566cf336bb2a84b8c9d001
Adham-Niazy <adhamniazy@gmail.com>
2024-11-21 02:05:58 +0200

    Totally Dynamic Table component

---
+++ src/components/BranchesTable.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: ["items", "headers"],
+};
+</script>
+
+<template>
+  <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+      <thead class="text-xs text-gray-700 border-b">
+        <tr>
+          <th v-for="header in headers" :key="header.value" scope="col" class="px-6 py-4">
+            {{ header.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-white hover:bg-gray-50 cursor-pointer" v-for="item in items" :key="item.id">
+          <td v-for="header in headers" :key="item.id + header.value" class="px-6 py-4">{{ item[header.value] }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
