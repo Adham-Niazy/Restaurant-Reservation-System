c58a38b356cc94017ea273ade88df39009761afd
Adham-Niazy <adhamniazy@gmail.com>
2024-11-21 14:57:54 +0200

    Turn local state management into vuex

--- src/views/ManageReservationsPage.vue
+++ src/views/ManageReservationsPage.vue
@@ -1,53 +1,43 @@
 <script>
+import { mapActions, mapGetters, mapState } from "vuex";
 import DynamicButton from "@/components/shared/DynamicButton.vue";
 import AddBranch from "@/components/AddBranch.vue";
 import BranchesTable from "@/components/BranchesTable.vue";
 import BasicModal from "@/components/shared/BasicModal.vue";
-import { listBranches } from "@/services/branch.services";
 export default {
   components: {
     DynamicButton,
     BranchesTable,
     BasicModal,
-    AddBranch
+    AddBranch,
   },
   computed: {
-    mappedTableContent() {
-      return this.branches.map((branch) => {
-        return {
-          id: branch.id,
-          ref: branch.reference,
-          branch: branch.name,
-          tablesCount:
-            branch?.sections?.reduce((acc, section) => {
-              return (
-                acc +
-                section.tables.filter((table) => table.accepts_reservations)
-                  .length
-              );
-            }, 0) || 0,
-          duration: `${branch.reservation_duration} Minutes`,
-        };
-      });
-    },
+    ...mapState(['selectedToAddBranch']),
+    ...mapGetters([
+      'getEnabledBranchesForReservations',
+      'mappedTableContent'
+    ]),
   },
   data() {
     return {
-      branches: [],
-      isOpenAddBranch: true,
+      isOpenAddBranch: false,
     };
   },
   async mounted() {
-    try {
-      const { data: branches } = await listBranches(true);
-      this.branches = branches.filter((branch) => branch.accepts_reservations);
-      console.log(this.branches);
-    } catch (e) {
-      console.log(e);
-    }
+    this.getBranches();
   },
   methods: {
-    onCloseAddBranch() {},
+    ...mapActions([
+      'getBranches',
+      'enableBranchForReservation'
+    ]),
+    onCloseAddBranch() {
+      this.isOpenAddBranch = false;
+    },
+    onProccedAddBranch() {
+      this.isOpenAddBranch = false;
+      this.enableBranchForReservation();
+    },
   },
 };
 </script>
@@ -56,7 +46,12 @@ export default {
   <div class="px-12 pt-12 bg-slate-200 min-h-[92vh]">
     <div class="bg-white rounded-md shadow-md">
       <div class="flex justify-end py-4 border-b border-[#EEE] px-4">
-        <DynamicButton btnStyle="secondary" @buttonClicked="isOpenAddBranch = true"> Add Branch </DynamicButton>
+        <DynamicButton
+          btnStyle="secondary"
+          @buttonClicked="isOpenAddBranch = true"
+        >
+          Add Branch
+        </DynamicButton>
       </div>
 
       <div>
@@ -72,11 +67,13 @@ export default {
       </div>
     </div>
     <BasicModal
+      :disabled="selectedToAddBranch === null"
       :isOpen="isOpenAddBranch"
       title="Add Branch"
-      @onClose="isOpenAddBranch = false"
+      @onClose="onCloseAddBranch"
+      @onProcced="onProccedAddBranch"
     >
-    <AddBranch></AddBranch>
-  </BasicModal>
+      <AddBranch></AddBranch>
+    </BasicModal>
   </div>
 </template>
