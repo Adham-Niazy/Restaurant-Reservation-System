6cf49242ea038209a9bf918343ed506b388cb830
Adham-Niazy <adhamniazy@gmail.com>
2024-11-22 23:15:57 +0200

    Add Disable branch for reservation functionality

--- src/components/EditBranch.vue
+++ src/components/EditBranch.vue
@@ -1,17 +1,19 @@
 <script>
 import { mapActions, mapMutations, mapState } from "vuex";
-import { getBranchDetails } from "@/services/branch.services";
+import { editBranch, getBranchDetails } from "@/services/branch.services";
 import InlineLoader from "@/components/shared/InlineLoader.vue";
 import BasicInput from "@/components/shared/BasicInput.vue";
 import { FORM_KEYS } from "@/store/state_keys";
 import { DESIRED_WEEK_ORDER } from "@/constants/days";
 import { deepClone } from "@/utils/objects";
 import TimeSlotsInput from "./shared/TimeSlotsInput.vue";
+import DynamicButton from "./shared/DynamicButton.vue";
 export default {
   components: {
     InlineLoader,
     BasicInput,
     TimeSlotsInput,
+    DynamicButton,
   },
   data() {
     return {
@@ -22,13 +24,24 @@ export default {
     ...mapState(["selectedToEditBranch", "form"]),
   },
   methods: {
-    ...mapActions(["editSpecificBranch"]),
+    ...mapActions(["editSpecificBranch", "resetStateVariables", "getBranches"]),
     ...mapMutations(["updateUIState", "updateFormState"]),
     onDurationChange(change) {
       this.updateFormState({
         key: FORM_KEYS.RESERVATION_DURATION,
         change,
       });
+    },
+    async onDisableBranch() {
+      try {
+        await editBranch(this.selectedToEditBranch, {
+          accepts_reservations: false,
+        });
+        await this.getBranches();
+        await this.resetStateVariables();
+      } catch (e) {
+        console.log(e);
+      }
     },
   },
   async mounted() {
@@ -44,7 +57,10 @@ export default {
       this.updateFormState({
         key: FORM_KEYS.RESERVATION_TIMES,
         change: Object.fromEntries(
-          DESIRED_WEEK_ORDER.map((key) => [key, deepClone(branchDetails?.reservation_times[key])])
+          DESIRED_WEEK_ORDER.map((key) => [
+            key,
+            deepClone(branchDetails?.reservation_times[key]) || [],
+          ])
         ),
       });
     } catch (e) {
@@ -56,12 +72,17 @@ export default {
 
 <template>
   <div class="flex flex-col gap-5" v-if="branchDetails">
-    <h2
-      class="border-violet-700 border-y bg-violet-700/20 text-violet-700 font-light p-3"
-    >
-      Branch working hours are {{ branchDetails.opening_from }} -
-      {{ branchDetails.opening_to }}
-    </h2>
+    <div class="flex flex-col items-end">
+      <DynamicButton @buttonClicked="onDisableBranch">
+        <span class="text-red-500">Disable Reservations</span>
+      </DynamicButton>
+      <h2
+        class="border-violet-700 border-y w-full bg-violet-700/20 text-violet-700 font-light p-3 mt-2"
+      >
+        Branch working hours are {{ branchDetails.opening_from }} -
+        {{ branchDetails.opening_to }}
+      </h2>
+    </div>
 
     <BasicInput
       name="branchReservationDuration"
