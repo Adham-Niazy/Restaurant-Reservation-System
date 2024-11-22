c592abbb987c85b1fdfb8e967228614c11994a31
Adham-Niazy <adhamniazy@gmail.com>
2024-11-23 01:35:39 +0200

    Handle Disable all button

--- src/components/layout/MainNavbar.vue
+++ src/components/layout/MainNavbar.vue
@@ -1,12 +1,34 @@
 <script>
 import DynamicButton from "@/components/shared/DynamicButton.vue";
+import { mapActions, mapGetters } from "vuex";
+import { editBranch } from "@/core/services/branch.services";
 export default {
+  data() {
+    return { isLoading: false };
+  },
   components: {
     DynamicButton,
   },
+  computed: {
+    ...mapGetters(["getEnabledBranchesForReservations"]),
+  },
   methods: {
-    handleClicked() {
-      console.log("CLICKED");
+    ...mapActions(['getBranches']),
+    async handleClicked() {
+      this.isLoading = true;
+      const branchesPromises = this.getEnabledBranchesForReservations.map((branch) =>
+        editBranch(branch.id, {
+          accepts_reservations: false,
+        })
+      );
+      try {
+        await Promise.all(branchesPromises);
+        await this.getBranches();
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.isLoading = false;
+      }
     },
   },
 };
@@ -21,6 +43,8 @@ export default {
       @buttonClicked="handleClicked"
       btnStyle="main"
       @click="handleClicked"
+      :loading="isLoading"
+      :disabled="isLoading"
     >
       Disable Reservation
     </DynamicButton>
